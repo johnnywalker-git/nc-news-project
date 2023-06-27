fbb555f24b91eda097209f8e6dc8a9e22b7d680e
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-06-27 11:42:24 +0100

    added endpoints to return an object of all endpoints which is dynamic.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const { getAllTopics } = require('./controllers/topics.controller')
+const { getAllEndpoints } = require('./controllers/endPoints.controller')
 const app = express()
 
 
+
 app.get("/api/topics", getAllTopics)
+
+app.get("/api", getAllEndpoints)
 
 module.exports = app
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,3 @@
-
 const topics = require("../db/data/test-data/topics")
 const topicsTestData  = require ("../db/data/test-data/topics")
 const { readAllTopics } = require("../models/topics.model")
