3ddbc9c64371af36da1414a7a17976ef5e5d75fd
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-06-27 08:43:58 +0100

    changes made to challenge 2 tests.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,40 +14,15 @@ beforeEach(() => {
   });
 
 describe("GET API/topics", () => {
-    test("Should return an array", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body}) => {
-            expect(body).toBeInstanceOf(Array)
-        })
-    })
-    test("Array should have objects inside of it.", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body}) => {
-            expect(body[0]).toBeInstanceOf(Object)
-        })
-    })
     test("Objects in the array should have the predefined keys", () => {
         return request(app)
         .get("/api/topics")
         .expect(200)
         .then(({body}) => {
-           body.map((treasure) => {
-            expect(treasure).toHaveProperty("slug")
-            expect(treasure).toHaveProperty("description")
-           })
-        })
-    })
-    test("Objects in the array should have two properties", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body}) => {
-           body.map((treasure) => {
-            expect(Object.keys(treasure).length).toBe(2)
+            expect(body.length > 0).toBe(true)
+                body.forEach((treasure) => {
+                expect(treasure).toHaveProperty("slug")
+                expect(treasure).toHaveProperty("description")
            })
         })
     })
