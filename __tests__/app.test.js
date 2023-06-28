b3a7f08e637627f3b58774ef7e0d442e977db56b
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-06-28 09:17:59 +0100

    main up to date.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const seed = require("../db/seeds/seed")
 const request = require('supertest')
 const db = require("../db/connection")
 const testData = require("../db/data/test-data/index")
-const endPoints = require("../endpoints.json")
 
 
 beforeEach(() => {
@@ -28,13 +27,44 @@ describe("GET API/topics", () => {
         })
     })
 })
-describe("GET /api", () => {
-    test("Returns an object describing all the available endpoints on the API", () => {
-        return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({body}) => {
-          expect(body).toEqual(endPoints)
-        })
-})
+describe("GET API/articles/:article_id", () => {
+  test("Returns an object with the specified keys.", () => {
+    return request(app)
+    .get("/api/articles/2")
+    .expect(200)
+    .then(({body}) => {
+      expect(body.finishedArticle).toHaveProperty("author")
+      expect(body.finishedArticle).toHaveProperty("title")
+      expect(body.finishedArticle).toHaveProperty("article_id")
+      expect(body.finishedArticle).toHaveProperty("body")
+      expect(body.finishedArticle).toHaveProperty("topic")
+      expect(body.finishedArticle).toHaveProperty("created_at")
+      expect(body.finishedArticle).toHaveProperty("votes")
+      expect(body.finishedArticle).toHaveProperty("article_img_url")
+    })
+  })
+  test("Returns an object with the specified item ID.", () => {
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then(({body}) => {
+      expect(body.finishedArticle.article_id).toBe(1)
+    })
+  })
+  test("Returns an error if nothing at the provided ID", () => {
+    return request(app)
+    .get("/api/articles/5976")
+    .expect(404)
+    .then(({body}) => {
+      expect(body.msg).toBe("Not Found")
+    })
+  })
+  test("Returns an error if given a bad ID.", () => {
+    return request(app)
+    .get("/api/articles/ABCD")
+    .expect(400)
+    .then(({body}) => {
+      expect(body.msg).toBe("Bad request")
+    })
+  })
 })
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { getAllTopics } = require('./controllers/topics.controller')
 const { getAllEndpoints } = require('./controllers/endPoints.controller')
+const { getArticle } = require("./controllers/articles.controller")
+
 const app = express()
 
 
@@ -8,5 +10,16 @@ const app = express()
 app.get("/api/topics", getAllTopics)
 
 app.get("/api", getAllEndpoints)
+
+
+app.get("/api/articles/:article_id", getArticle)
+
+app.use((err, req, res, next) => {
+    if(err.code === "22P02")
+    {
+    res.status(400).send({msg : "Bad request"})
+    } else
+    res.status(404).send({msg : "Not Found"})
+})
 
 module.exports = app
